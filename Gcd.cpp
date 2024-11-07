319762fe501c900b08ea5629a6b7c9d692845c84
Aman Raj <135144595+Amanraj0149@users.noreply.github.com>
2024-11-07 14:53:26 +0530

    Update Gcd.cpp

--- Gcd.cpp
+++ Gcd.cpp
@@ -13,3 +13,19 @@ public class hcf{
         sc.close();
     }
 }
+
+import java.util.Scanner;
+public class hcf{
+     public static int gcd(int a, int b){
+            if(b==0) return a;
+            return gcd(b,a%b);
+        }
+    public static void main (String[] args){
+        Scanner sc=new Scanner(System.in);
+       int a=sc.nextInt();
+       int b=sc.nextInt();
+       int ans=gcd(a,b);
+       System.out.println(ans);
+        sc.close();
+    }
+}
