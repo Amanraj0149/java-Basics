819a567c1a45837e17066e09887c76401d95e7fc
Aman Raj <135144595+Amanraj0149@users.noreply.github.com>
2024-11-07 15:22:23 +0530

    Create CountIntegersize.java

---
+++ CountIntegersize.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class CountIntegersize{
+  public static void main( String[] args){
+    Scanner sc=new Scanner(System.in);
+    int n=sc.nextInt();
+    int ans= String.valueOf(n).length();
+    System.out.println(ans);
+    sc.close();
+  }
+}
